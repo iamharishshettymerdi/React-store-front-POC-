51628f7df9c6a9f4e6371a6d1c7294f04f294a39
iamharishshettymerdi <harisha@litmus7.com>
2023-01-12 11:07:51 +0530

    Index file modified

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ import VisibilityOffOutlinedIcon from '@material-ui/icons/VisibilityOffOutlined'
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import Tooltip from '@material-ui/core/Tooltip';
 //=======================================
-
+import Content from '../components/OfferComponents/Content'
 import justdropped from '../local-json/justdropped.json'
 
 
@@ -111,7 +111,17 @@ export default function Index(lazyProps) {
             <MultiCarousel productdata={productdata}/>
            
               <MultiCarousel productdata={justdropped}/>
-            
+    
+                <Content/>
+
+                <div style={{ display: 'flex',
+              width: '82%',
+              margin: 'auto'}}>
+              <GiftandPromotion style={{display:'inline-block'}} imageurl={'https://www.sephora.com/contentimages/2022-12-26-holiday-launch-site-mobile-desktop-rwd-home-page-marketing-banner-gift-card-redeem-800x256-us-can.jpg?imwidth=400'} heading={'Got A Gift Card?'} description={'Redeem now and shop our sale.'} backgroundcolor={'darkgoldenrod'}/>
+              <GiftandPromotion style={{display:'inline-block'}} imageurl={'https://www.sephora.com/contentimages/2022-12-30-jo-malone-london-roses-collection-site-desktop-mweb-home-page-rwd-marketing-banner-800x256-en-us-can.jpg?imwidth=400'} heading={'NEW Jo Malone London'} description={'Limited-edition floral scents with notes of scarlet velvet rose, honeycomb, and vanilla.'} backgroundcolor={'antiquewhite'}/>
+              <GiftandPromotion style={{display:'inline-block'}} imageurl={'https://www.sephora.com/contentimages/homepage/092022/Homepage/RWD/2022-09-30-hp-marketing-banner-convenience-hub-same-day-unlimited-us-ca-rwd-slice.jpeg?imwidth=400'} heading={'Try Same-Day Unlimited for FREE'} description={'Want to get your beauty in as little as two hours? Enjoy a 30-day subscription trial.'} backgroundcolor={'lightsteelblue'}/>
+            </div>
+       
            
             <div style={{display: 'flex',marginTop: '100px'}}>
              <h2 style={{width: '125px'}}>Featured Categories</h2> {featuredcategories.map((element)=>(<CategoriesComponent  key={element.categoryname} data={element}/>))}
@@ -119,6 +129,7 @@ export default function Index(lazyProps) {
             <div style={{display: 'flex'}}>
             <h2  style={{width: '125px'}}>Need a Little Guidance?</h2> {Guidlines.map((element)=>(<CategoriesComponent  key={element.guidename} data={element}/>))}
             </div>
+           
             <div style={{ display: 'flex',
               width: '82%',
               margin: 'auto'}}>
@@ -127,6 +138,7 @@ export default function Index(lazyProps) {
               <GiftandPromotion style={{display:'inline-block'}} imageurl={'https://www.sephora.com/contentimages/homepage/092022/Homepage/RWD/2022-09-30-hp-marketing-banner-convenience-hub-same-day-unlimited-us-ca-rwd-slice.jpeg?imwidth=400'} heading={'Try Same-Day Unlimited for FREE'} description={'Want to get your beauty in as little as two hours? Enjoy a 30-day subscription trial.'} backgroundcolor={'lightsteelblue'}/>
             </div>
 
+           
             {/* <button onClick={handleOpen}>Open modal</button> */}
             <Modal
         open={open}
